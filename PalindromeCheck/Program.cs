595eb878d2922dfde2e2768255cae3d472dc3d7e
Baha <bahabayar@hotmail.com>
2021-04-21 01:01:05 +0300

    Add project files.

---
+++ PalindromeCheck/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class MainClass
+{
+
+    public static string StringChallenge(string firstStr)
+    {
+
+        char[] palindromeArray = firstStr.ToCharArray();
+        Array.Reverse(palindromeArray);
+        string second = new string(palindromeArray);
+
+        return second;
+    }
+
+    static void Main()
+    {
+        string firstStr = Convert.ToString(Console.ReadLine());
+        firstStr = firstStr.Replace(" ", "");
+        string secondStr = StringChallenge(firstStr);
+
+
+        if (firstStr.Equals(secondStr))
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            Console.WriteLine("false");
+        }
+        Console.ReadLine();
+    }
+
+
+}
